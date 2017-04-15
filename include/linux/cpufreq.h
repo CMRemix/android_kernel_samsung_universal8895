5fe5955fa49280081f2a02c19d401fa21c2e1d0e
mydongistiny <jaysonedson@gmail.com>
2017-04-14 23:31:35 -0700

    cpufreq: cultivation: bring in initial release
     -based off of caf 4.4 commits
     -uses per-pcu timers
     -use state_notifier for screen off timer
        option to set different timer rate when screen off
     -improrted fastlane with threshold from blu_active
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -548,6 +548,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
+extern struct cpufreq_governor cpufreq_gov_cultivation;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
@@ -564,6 +567,7 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 extern struct cpufreq_governor schedutil_gov;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&schedutil_gov)
 #endif
+
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
