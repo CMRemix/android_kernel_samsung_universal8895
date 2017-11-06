f52c686d9a1ad48075151e18e2461e0335a8fd53
ZION959 <minhdtran43@gmail.com>
2017-11-06 12:30:26 -0700

    revert: revert: boeffla_wl_blocker: update to wakelock blocker driver v1.1.0
    There are now two lists:
    - the previously existing list of user defined wakelocks to block
    - a new list called "wakelock_blocker_default" which comes prepopulated with the most common
      and safe wakelocks to block:
    
        qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK
    
    A combination of both wakelock lists will be blocked finally.
    boeffla_wl_blocker: update to wakelock blocker driver v1.0.1
    
    - currently active wakelocks on the list are forcefully killed
    boeffla_wl_blocker: add generic wakelock blocker driver v1.0.0
    
    Based on ideas of FranciscoFranco's non-generic driver.
    
    Sysfs node:
    
    	/sys/class/misc/boeffla_wakelock_blocker/wakelock_blocker
    
    		- list of wakelocks to be blocked, separated by semicolons
    
    	/sys/class/misc/boeffla_wakelock_blocker/debug
    
    		- write: 0/1 to switch off and on debug logging into dmesg
    		- read:  get current driver internals
    
    	/sys/class/misc/boeffla_wakelock_blocker/version
    
    		- show driver version
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: ZION959 <minhdtran43@gmail.com>
    
    (reverted from commit e0b3adeb62aa7376c57041fdf34e155bea557098)
    (reverted from commit c62ba742387c600cb2c77145c0a3a7242f14c01d)
    (reverted from commit 7036576cd7f8300a291d9db5045dabb0fabd5066)
    (reverted from commit 73ed24a72fc5918ff0400af9d574f50e2fe34b1c)

---
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -0,0 +1,23 @@
+/*
+ * Author: andip71, 01.09.2017
+ *
+ * Version 1.1.0
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
+
+#define LIST_WL_DEFAULT				"sensorhub_wl;ssp_wl;lli_pm_wl;radio_interface_wl;umts_ipc0_wl;power_manager_service_wl;wlan_rx_wake_ws;wlan_ctrl_wake_ws;wlan_wake_ws;bluedroid_timer_ws;wl_sysovlo;wl_divide_bt;wl_divide;wl_nfc;wl_motion;wl_prox;wl_tilt;wl_grip;wl_pickup;wl_humi;wl_sensorhub;wl_ssp;dwc3-otg;usb_notify;mmc0_detect;bbd_wake_lock;NETLINK"
+
+#define LENGTH_LIST_WL				255
+#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
