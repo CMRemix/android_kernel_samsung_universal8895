dwander/Linaro-base

devfreq: Add generic voltage control / Adapt to OPPv2
Signed-off-by: DQ <dqstyle@gmail.com>

drivers/devfreq/devfreq.c
@@ -18,15 +18,24 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
-#include <linux/pm_opp.h>
 #include <linux/pm_qos.h>
 #include <linux/devfreq.h>
 #include <linux/workqueue.h>
 #include <linux/platform_device.h>
+#include <linux/sysfs_helpers.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/hrtimer.h>
 #include "governor.h"
+#include "../base/power/opp/opp.h"
+
+#ifdef CONFIG_SOC_EXYNOS8890
+#define DF_MAX_VOLT		900000
+#define DF_MIN_VOLT		500000
+#define DF_VOLT_STEP		6250
+#else
+#error "Please define core voltage ranges for current SoC."
+#endif
 
 static struct class *devfreq_class;
 
@@ -1082,6 +1091,70 @@ static ssize_t time_in_state_show(struct device *dev,
 }
 static DEVICE_ATTR_RO(time_in_state);
 
+static ssize_t volt_table_store(struct device *d, struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = _find_device_opp(dev);
+	struct dev_pm_opp *temp_opp;
+	int u[15];
+	int rest, t, i = 0;
+
+	if ((t = read_into((int*)&u, 15, buf, count)) < 0)
+		return -EINVAL;
+
+	mutex_lock(&df->lock);
+	if (t == 2 && 15 != 2) {
+		temp_opp = dev_pm_opp_find_freq_exact(dev, u[0], true);
+		if(IS_ERR(temp_opp))
+			return -EINVAL;
+
+		if ((rest = (u[1] % DF_VOLT_STEP)) != 0) 
+			u[1] += DF_VOLT_STEP - rest;
+		
+		sanitize_min_max(u[1], DF_MIN_VOLT, DF_MAX_VOLT);
+		temp_opp->u_volt = u[1];
+	} else {
+		list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+			if (temp_opp->available) {
+				if ((rest = (u[i] % DF_VOLT_STEP)) != 0) 
+					u[i] += DF_VOLT_STEP - rest;
+				
+				sanitize_min_max(u[i], DF_MIN_VOLT, DF_MAX_VOLT);
+				temp_opp->u_volt = u[i++];
+			}
+		}
+	}
+
+	mutex_unlock(&df->lock);
+
+	return count;
+}
+
+static ssize_t volt_table_show(struct device *d, 
+				struct device_attribute *attr, char *buf)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = _find_device_opp(dev);
+	struct dev_pm_opp *temp_opp;
+	int len = 0;
+
+	if (IS_ERR_OR_NULL(dev_opp))
+		return -EINVAL;
+
+	list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+		if (temp_opp->available)
+			len += sprintf(buf + len, "%lu %lu\n",
+					dev_pm_opp_get_freq(temp_opp),
+					dev_pm_opp_get_voltage(temp_opp));
+	}
+
+	return len;
+}
+static DEVICE_ATTR_RW(volt_table);
+
 static struct attribute *devfreq_attrs[] = {
 	&dev_attr_governor.attr,
 	&dev_attr_available_governors.attr,
@@ -1093,6 +1166,7 @@ static struct attribute *devfreq_attrs[] = {
 	&dev_attr_max_freq.attr,
 	&dev_attr_trans_stat.attr,
 	&dev_attr_time_in_state.attr,
+	&dev_attr_volt_table.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(devfreq);

CMRemix/android_kernel_samsung_universal8895

Signed-off-by: ZION959 <minhdtran43@gmail.com>

drivers/devfreq/devfreq.c
@@ -18,15 +18,24 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
-#include <linux/pm_opp.h>
 #include <linux/pm_qos.h>
 #include <linux/devfreq.h>
 #include <linux/workqueue.h>
 #include <linux/platform_device.h>
+#include <linux/sysfs_helpers.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/hrtimer.h>
 #include "governor.h"
+#include "../base/power/opp/opp.h"
+
+#ifdef CONFIG_SOC_EXYNOS8895
+#define DF_MAX_VOLT		900000
+#define DF_MIN_VOLT		500000
+#define DF_VOLT_STEP		6250
+#else
+#error "Please define core voltage ranges for current SoC."
+#endif
 
 #include <soc/samsung/exynos-devfreq.h>
 #include <soc/samsung/exynos-dm.h>
@@ -1132,6 +1141,70 @@ static ssize_t time_in_state_show(struct device *dev,
 }
 static DEVICE_ATTR_RO(time_in_state);
 
+static ssize_t volt_table_store(struct device *d, struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = _find_device_opp(dev);
+	struct dev_pm_opp *temp_opp;
+	int u[15];
+	int rest, t, i = 0;
+
+	if ((t = read_into((int*)&u, 15, buf, count)) < 0)
+		return -EINVAL;
+
+	mutex_lock(&df->lock);
+	if (t == 2 && 15 != 2) {
+		temp_opp = dev_pm_opp_find_freq_exact(dev, u[0], true);
+		if(IS_ERR(temp_opp))
+			return -EINVAL;
+
+		if ((rest = (u[1] % DF_VOLT_STEP)) != 0) 
+			u[1] += DF_VOLT_STEP - rest;
+		
+		sanitize_min_max(u[1], DF_MIN_VOLT, DF_MAX_VOLT);
+		temp_opp->u_volt = u[1];
+	} else {
+		list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+			if (temp_opp->available) {
+				if ((rest = (u[i] % DF_VOLT_STEP)) != 0) 
+					u[i] += DF_VOLT_STEP - rest;
+				
+				sanitize_min_max(u[i], DF_MIN_VOLT, DF_MAX_VOLT);
+				temp_opp->u_volt = u[i++];
+			}
+		}
+	}
+
+	mutex_unlock(&df->lock);
+
+	return count;
+}
+
+static ssize_t volt_table_show(struct device *d, 
+				struct device_attribute *attr, char *buf)
+{
+	struct devfreq *df = to_devfreq(d);
+	struct device *dev = df->dev.parent;
+	struct device_opp *dev_opp = _find_device_opp(dev);
+	struct dev_pm_opp *temp_opp;
+	int len = 0;
+
+	if (IS_ERR_OR_NULL(dev_opp))
+		return -EINVAL;
+
+	list_for_each_entry_rcu(temp_opp, &dev_opp->opp_list, node) {
+		if (temp_opp->available)
+			len += sprintf(buf + len, "%lu %lu\n",
+					dev_pm_opp_get_freq(temp_opp),
+					dev_pm_opp_get_voltage(temp_opp));
+	}
+
+	return len;
+}
+static DEVICE_ATTR_RW(volt_table);
+
 static struct attribute *devfreq_attrs[] = {
 	&dev_attr_governor.attr,
 	&dev_attr_available_governors.attr,
@@ -1143,6 +1216,7 @@ static struct attribute *devfreq_attrs[] = {
 	&dev_attr_max_freq.attr,
 	&dev_attr_trans_stat.attr,
 	&dev_attr_time_in_state.attr,
+	&dev_attr_volt_table.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(devfreq);